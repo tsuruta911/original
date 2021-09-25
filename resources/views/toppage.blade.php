3aa0f51c6d413ee5c69400a5b9af53b30d96c5b8
EC2 Default User <ec2-user@ip-172-31-10-156.us-east-2.compute.internal>
2021-09-25 08:17:43 +0000

    original toppageiro

--- resources/views/toppage.blade.php
+++ resources/views/toppage.blade.php
@@ -3,24 +3,15 @@
 
 @section('content')
     <div class="container">
+        <h2>MeeTTennis</h2>
+        <div class="col-md-6">
+            <p>MeeTTennisとは</p>
+            <p>対戦相手が見つからない・試合参加者が集まらない</p>
+            <p>テニスしたい人同士をマッチングするサイト</p>
+        </div>
         <div class="row">
-            <div class="col-md-8 mx-auto">
-                <h2>MeeTTennis</h2>
-                <form action="{{ action('Admin\TennisController@create') }}" method="post" enctype="multipart/form-data">
-                    
-                    <div class="form-group row">
-                        <label class="col-md-9" for="title">MeeTTennisとは対戦相手が見つからない・試合参加者が集まらないテニスしたい人同士をマッチングするサイト</label>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-2" for="body"></label>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-2" for="title">画像</label>
-                    </div>
-                    {{ csrf_field() }}
-                    <input type="submit" class="btn btn-primary" value="更新">
-                </form>
-            </div>
+            <p class="mr-4"><a class="d-block rounded-circle p-4 bg-info" href="{{ url('/entry') }}">エントリーする</a></p>
+            <p class="mr-4"><a class="d-block rounded-circle p-4 bg-info"href="{{ url('/#') }}">テニスマッチングを主催する</a></p>
         </div>
     </div>
 @endsection
